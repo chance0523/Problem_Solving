94b0896df44112c1c438e7e7460d7709c98588d2
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-09-24 19:22:20 +0900

    1

--- 1.py
+++ 1.py
@@ -1,6 +1,5 @@
-a = list(map(int, input().split()))
-for c in a:
-    if c != 0:
-        print(c)
-    else:
-        break
+n, m = map(int, input().split())
+
+for i in range(n):
+    for j in range(m):
+        print("%d %d" % (i+1, j+1))
