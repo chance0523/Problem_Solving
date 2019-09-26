2d4c66059b41418afae7f364cd110312e3380af5
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-09-26 20:03:25 +0900

    1

--- greedy/coin0.py
+++ greedy/coin0.py
@@ -1,1 +1,11 @@
 N, K = map(int, input().split())
+coin_val = [int(input()) for _ in range(N)]
+
+coin_num = 0
+coin_val.reverse()
+
+for i in coin_val:
+    if i <= K:
+        coin_num += (K//i)
+        K -= (K//i)*i
+print(coin_num)
