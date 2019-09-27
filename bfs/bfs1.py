2deff7dce536c2c04dd25de8032d85e665eedf47
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-09-27 17:10:34 +0900

    1

--- bfs/bfs1.py
+++ bfs/bfs1.py
@@ -9,17 +9,19 @@ visited[v] = 1
 queue = [0]*(n+1)
 queue[0] = v
 front = 0
+next_node=front
 
 while queue[front] and front != n:
     nd = queue[front]
-    front = 1
     for i in range(1, n+1):
         if board[nd][i] and not visited[i]:
             visited[i] = 1
+            front+=1
             queue[front] = i
-            # print(queue)
-            front += 1
+ #           print(queue)
+    next_node+=1
+    front=queue[next_node]
 
-# print(board)
+#print(board)
 print(*queue[0:n])
-# print(visited)
+#print(visited)
