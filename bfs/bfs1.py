bbdf985d9f2ce17b1d7200fe0140ecf76f72c3fb
samuel950523 <51807128+samuel950523@users.noreply.github.com>
2019-09-27 16:57:47 +0900

    1

--- bfs/bfs1.py
+++ bfs/bfs1.py
@@ -1,33 +1,25 @@
-N,M,V=map(int,input().split())
-matrix=[[0]*(N+1) for _ in range(N+1)]
+n, m, v = map(int, input().split())
+board = [[0] * (n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    board[a][b] = board[b][a] = 1
 
-for _ in range(M):
-    link=list(map(int,input().split()))
-    matrix[link[0]][link[1]] = 1
-    matrix[link[1]][link[0]] = 1
+visited = [0]*(n+1)
+visited[v] = 1
+queue = [0]*(n+1)
+queue[0] = v
+front = 0
 
-def bfs(start):
-    queue=[start]
-    foot_prints=[start]
-    while queue:
-        current_node=queue.pop(0)
-        for search_node in range(len(matrix[current_node])):
-            if matrix[current_node][search_node] and search_node not in foot_prints:
-                foot_prints+=[search_node]
-                queue+=[search_node]
-    return foot_prints
+while queue[front] and front != n:
+    nd = queue[front]
+    front = 1
+    for i in range(1, n+1):
+        if board[nd][i] and not visited[i]:
+            visited[i] = 1
+            queue[front] = i
+            # print(queue)
+            front += 1
 
-def bfs2(graph, start):
-    visited = []
-    queue = [start]
-
-    while queue:
-        n = queue.pop(0)
-        if n not in visited:
-            visited.append(n)
-            queue += graph[n] - set(visited)
-    return visited
-
-print(*bfs2(matrix,V))
-print(*bfs(V))
-print(matrix)
+# print(board)
+print(*queue[0:n])
+# print(visited)
