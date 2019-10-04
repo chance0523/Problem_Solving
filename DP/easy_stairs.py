dc0ba5d6b9387e5812ff70816570a274938e15c5
samuel950523 <samuel95@naver.com>
2019-10-05 00:03:39 +0900

    1

--- DP/easy_stairs.py
+++ DP/easy_stairs.py
@@ -1,12 +1,20 @@
 N = int(input())
-ans = [0 for i in range(101)]
-
-ans[1] = 9
-ans[2] = 17
-# 마지막 두 자리가 10일때는 101, 89일때는 898밖에 안 이어짐
-ans[3] = (17-2)*2+2
-
-for i in range(4, N+1):
-    ans[i] = (ans[i-1]-2)*2+2
-
-print(ans[N] % 1000000000)
+ans = [1]*10
+ans[0]=0
+i=1
+while i<N:
+    next=[
+        ans[1],
+        ans[0]+ans[2],
+        ans[1]+ans[3],
+        ans[2]+ans[4],
+        ans[3]+ans[5],
+        ans[4]+ans[6],
+        ans[5]+ans[7],
+        ans[6]+ans[8],
+        ans[7]+ans[9],
+        ans[8]
+    ]
+    ans=next
+    i+=1
+print(sum(ans)%1000000000)
