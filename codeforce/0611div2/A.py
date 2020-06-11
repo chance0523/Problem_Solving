5d4a5fb2358995b7d89f345572481f1b8d5eaf34
samuel950523 <samuel95@naver.com>
2020-06-12 00:51:30 +0900

    0612

---
+++ codeforce/0611div2/A.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    ans = 0
+
+    print(ans)
---
+++ codeforce/0611div2/B.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+
+t = int(input())
+for i in range(t):
+    n, x, m = map(int, input().split())
+    nList = [0 for k in range(n)]
+    nList[x] = 1
+    for j in range(m):
+        l, r = map(int, input().split())
