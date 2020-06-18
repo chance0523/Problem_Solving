d2dcb247159b1579b4c03fdcef582af1d0bbff17
samuel950523 <samuel95@naver.com>
2020-06-18 18:24:56 +0900

    0618

--- BOJ/postOnBlog.py
+++ BOJ/postOnBlog.py
@@ -4,9 +4,9 @@ date = str(datetime.now())[:10]
 
 # -----------------------------
 
-file = '1026_treasure'
-category = 'sort'
-tier = 'silver4'
+file = '11286_absheap'
+category = 'heap'
+tier = 'silver1'
 
 
 # ---------------------------
