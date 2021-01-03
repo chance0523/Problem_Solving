26de110cd332e871dcf118ee9a01933a801ea92a
samuel950523 <samuel95@naver.com>
2021-01-03 18:02:16 +0900

    0103

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,37 +1,21 @@
-import sys
-
-
 n = int(input())
-i = 0  # S(i)
-s = 3  # 초기값
-k = 4  # 가운데 1 + k+2 (mooo)
+# n<=9로 작아서 brute force로도 가능하다.
 
-while s <= n:  # n을 넘어가기 직전까지
-    i += 1
-    s = s + k + s  # 전체 문자열 길이 업데이트
-    k += 1
 
-k -= 1  # 하나를 빼줘야 인덱스를 맞출 수 있다.
+def func(l, num):
+    global ans
+    if l == n:  # n자리수 다 채웠으면
+        if num % 3 == 0:  # 3의 배수인지 확인
+            ans += 1
+        return
 
-while i >= 0:
-    rest = (s - k) // 2  # 가운데 moooo를 뺀 나머지 한쪽. S(k-1)의 길이
-    # 왼쪽 S(k-1)
-    if n <= rest:
-        s = rest  # 탐색할 문자열의 길이를 rest로 바꿔줌
-        k -= 1  # S(k-1)
+    func(l + 1, num + 0)  # 다음 수 채워줌
+    func(l + 1, num + 1)
+    func(l + 1, num + 2)
 
-    # 가운데 moooo...
-    elif rest < n <= rest + k:
-        if n == rest + 1:  # 이게 가운데의 첫번째 인덱스
-            print('m')
-            break
-        else:
-            print('o')
-            break
 
-    # 오른쪽 S(k-1)
-    else:
-        n = n - rest - k  # 왼쪽 S(k-1)과 가운데 mooo...를 빼준 상태로 업데이트
-        s = rest  # 탐색할 문자열의 길이를 rest로 바꿔줌
-        k -= 1  # S(k-1)
-    i -= 1
+ans = 0
+func(1, 1)
+func(1, 2)
+
+print(ans)
