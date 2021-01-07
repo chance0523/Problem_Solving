919d72bc5269c556b8c9dbb60b34189c4e66681f
samuel950523 <samuel95@naver.com>
2021-01-07 18:06:24 +0900

    0107

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,6 +1,12 @@
-import sys
-input = sys.stdin.readline
+def getLCM(a, b):
+    M = max(a, b)
+    cnt = M
+    while True:
+        if not M % a and not M % b:
+            return M
+        M += cnt
 
-n = int(input())
-nList = sorted(list(map(int, input().split())))
-print(*nList)
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(getLCM(a, b))
