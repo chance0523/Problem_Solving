4e0d2f0f59aa352b4e590518f2c9353e2f0da06a
samuel950523 <samuel95@naver.com>
2021-01-16 08:26:22 +0900

    0116

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,30 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-a = [list(map(int, input().rstrip())) for _ in range(n)]
-b = [list(map(int, input().rstrip())) for _ in range(n)]
+m = int(input())
+n = int(input())
+nList = [i**2 for i in range(101)]  # 완전 제곱수들 저장
 
+sum = 0
+ans = 10001
+for i in range(1, len(nList)):  # 0 제외
+    if m <= nList[i] <= n:  # 조건 만족
+        sum += nList[i]
+        ans = min(ans, nList[i])
 
-def isEqual():
-    for i in range(n):
-        for j in range(m):
-            if a[i][j] != b[i][j]:
-                return False
-    return True
-
-
-ans = 0
-for i in range(n - 2):
-    for j in range(m - 2):
-        if a[i][j] != b[i][j]:  # 다르다면
-            # 1->0, 0->1
-            for x in range(i, i + 3):
-                for y in range(j, j + 3):
-                    a[x][y] = 1 - a[x][y]
-            ans += 1
-
-if isEqual():
-    print(ans)
-else:
+if sum == 0:
     print(-1)
+else:
+    print(sum)
+    print(ans)
