d96cfdf71785a839c24091efa1c0206391f0a924
samuel950523 <samuel95@naver.com>
2021-02-20 21:46:39 +0900

    0220

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,19 +1,9 @@
-import sys
-input = sys.stdin.readline
+n = input()
+nList = list(input().split())
 
-m = int(input())
-n = int(input())
-nList = [i**2 for i in range(101)]  # 완전 제곱수들 저장
+ans = 0
+for i in range(5):
+    if nList[i][-1] == n:
+        ans += 1
 
-sum = 0
-ans = 10001
-for i in range(1, len(nList)):  # 0 제외
-    if m <= nList[i] <= n:  # 조건 만족
-        sum += nList[i]
-        ans = min(ans, nList[i])
-
-if sum == 0:
-    print(-1)
-else:
-    print(sum)
-    print(ans)
+print(ans)
