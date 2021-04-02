1c831f240e63b977c12a00ad4c34921542431bda
samuel950523 <samuel95@naver.com>
2021-04-03 01:12:21 +0900

    0403

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,9 +1,19 @@
-n = input()
-nList = list(input().split())
+for _ in range(int(input())):
+    n = int(input())
+    nList = list(map(int, input().split()))
+    ans = 0
 
-ans = 0
-for i in range(5):
-    if nList[i][-1] == n:
-        ans += 1
+    while True:
+        if not nList:
+            break
 
-print(ans)
+        cur = nList.pop()
+
+        l = len(nList)
+        for i in range(l - 1, -1, -1):
+            if cur >= nList[i]:
+                ans += (cur - nList[i])
+                nList.pop()
+            else:
+                break
+    print(ans)
