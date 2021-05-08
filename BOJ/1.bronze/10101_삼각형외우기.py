7b417e3634d0df462ba32339d7407dcfd5d87c29
samuel950523 <samuel95@naver.com>
2021-05-08 11:12:29 +0900

    0508

--- BOJ/1.bronze/10101_삼각형외우기.py
+++ BOJ/1.bronze/10101_삼각형외우기.py
@@ -7,9 +7,9 @@ c = int(input())
 
 if a + b + c != 180:
     print('Error')
-elif a == b == c == 60:
+elif a == b == c:
     print('Equilateral')
-elif a + b + c == 180 and a == b or b == c or c == a:
+elif a == b or b == c or c == a:
     print('Isosceles')
-elif a + b + c == 180 and a != b and b != c and c != a:
+else:
     print('Scalene')
