2e93dafc29bf6295154a0a31eecf340801a457c3
서정준 <samuel95@naver.com>
2021-06-06 01:14:05 +0900

    0606

---
+++ BOJ/1.bronze/10768_특별한날.py
@@ -0,0 +1,16 @@
+# 10768번. 특별한 날
+
+
+month = int(input())
+day = int(input())
+if month < 2:
+    print("Before")
+elif month > 2:
+    print("After")
+else:
+    if day > 18:
+        print("After")
+    elif day < 18:
+        print("Before")
+    else:
+        print("Special")
