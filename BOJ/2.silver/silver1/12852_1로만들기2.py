c46fa585a28b76c8640a73366474347124122923
서정준 <samuel95@naver.com>
2021-07-20 00:27:37 +0900

    0720

--- BOJ/2.silver/silver1/12852_1로만들기2.py
+++ BOJ/2.silver/silver1/12852_1로만들기2.py
@@ -1,5 +1,11 @@
 # 12852번. 1로 만들기 2
 
+n = int(input())
+dp = [[0, []] for _ in range(10**6 + 1)]
+dp[1] = [0, [1]]
+dp[2] = [1, [1, 2]]
+dp[3] = [1, [1, 3]]
+
 
 def func(a, b):
     dp[a][0] = dp[b][0] + 1  # 횟수 늘려주기
@@ -8,24 +14,21 @@ def func(a, b):
     dp[a][1].append(a)
 
 
-n = int(input())
-dp = [[0, []] for _ in range(1000000 + 1)]
-# dp = [[0, []] for _ in range(n + 1)] # 런타임에러
-dp[0] = [0, [0]]
-dp[1] = [1, [1]]
-dp[2] = [1, [1, 2]]
-dp[3] = [1, [1, 3]]
-
-
 for i in range(4, n + 1):
     if i % 3 == 0:
-        if dp[i // 3][0] <= dp[i - 1][0]:
-            func(i, i//3)
+        if i % 2 != 0:
+            if dp[i // 3][0] <= dp[i - 1][0]:
+                func(i, i // 3)
+            else:
+                func(i, i - 1)
         else:
-            func(i, i-1)
+            if dp[i // 3][0] <= dp[i // 2][0]:
+                func(i, i // 3)
+            else:
+                func(i, i // 2)
     elif i % 2 == 0:
         if dp[i // 2][0] <= dp[i - 1][0]:
-            func(i, i//2)
+            func(i, i // 2)
         else:
             func(i, i - 1)
     else:
