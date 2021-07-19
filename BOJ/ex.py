7cc729325739f633bd945a64157b3a56a8b4dec6
서정준 <samuel95@naver.com>
2021-07-19 19:00:30 +0900

    0719

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,13 +1,2 @@
-a, b, c = map(int, input().split())
-ans = 0
-if a == b == c:
-    ans = 10000 + a * 1000
-elif a == b:
-    ans = 1000 + a * 100
-elif b == c:
-    ans = 1000 + b * 100
-elif c == a:
-    ans = 1000 + c * 100
-else:
-    ans = max(a, b, c) * 100
-print(ans)
+input()
+print(int(input())*int(input()))
