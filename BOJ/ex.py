62010e7077621a60d01a3f80b9b18588d6cc9a89
chance0523 <samuel95@naver.com>
2021-08-21 11:16:11 +0900

    0821

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,18 +1,8 @@
 n = int(input())
-ans = 0
-l = 0
-r = 1
-s = 1
-while l<=n and r<=n:
-    if s == n:
-        r += 1
-        s = s - l + r
-        l += 1
-        ans += 1
-    elif s > n:
-        s -= l
-        l += 1
-    elif s < n:
-        r += 1
-        s += r
-print(ans)
+s = []
+for i in range(n):
+    sL = list(input().split())
+    s.append(sL)
+    s[i][0],s[i][1] = s[i][1],s[i][0]
+s.sort()
+print(s[0][1])
