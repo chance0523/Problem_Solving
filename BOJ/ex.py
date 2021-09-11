c1146be71982092d290ef679bf15a76c5f6d11e7
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-12 01:30:16 +0900

    0912

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,26 +1,13 @@
-n=int(input())
-name = input().rstrip()
-ans = 0
-for _ in range(n):
-    old = input().rstrip()
-    flag = False
-    for gan in range(1, len(old)):
-        if flag:
-            break
-        flag = True
-        for s in range(len(old)-gan*(len(name)-1)):
-            cnt = 0
-            for i in range(len(name)):
-                if old[s + gan*i] != name[i]:
-                    flag = False
-                    break
-                else:
-                    cnt += 1
-                if cnt == len(name):
-                    flag = True
-                    break
-            if flag:
-                ans += 1
-                break
-
-print(ans)
+n = int(input())
+nList = list(map(int,input().split()))
+y = 0 
+m = 0
+for j in nList:
+    y += (j // 30) * 10 + 10
+    m += (j // 60) * 15 + 15
+if y == m:
+    print('Y','M',y)
+elif y > m:
+    print('M', m)
+else:
+    print('Y', y)
