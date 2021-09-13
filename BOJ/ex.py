53d3215a584b985e852cda36f3140cc418a05e38
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-13 20:38:22 +0900

    0913

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,13 +1,16 @@
-n = int(input())
-nList = list(map(int,input().split()))
-y = 0 
-m = 0
-for j in nList:
-    y += (j // 30) * 10 + 10
-    m += (j // 60) * 15 + 15
-if y == m:
-    print('Y','M',y)
-elif y > m:
-    print('M', m)
-else:
-    print('Y', y)
+while True:
+    a, b = map(int,input().split())
+    if a+b == 0:
+        break
+    # 두 수가 같은 경우는 없다.
+    ans = [0,0]
+    if b%a == 0:
+        ans[0] = 1
+    if a%b==0:
+        ans[1] = 1
+    if ans[0]:
+        print('factor')
+    elif ans[1]:
+        print('multiple')
+    else:
+        print('neither')
--- codeforce/B.py
+++ codeforce/B.py
@@ -3,70 +3,7 @@ input = sys.stdin.readline
 
 
 for _ in range(int(input())):
-    n = int(input())
     s = input().rstrip()
-    ans = [[0 for i in range(n)] for i in range(n)]
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                ans[i][j] = 'X'
-                continue
-            if s[i] == '1' and s[j] == '1':
-                ans[i][j] = '='
-            elif s[i] == '1' and s[j] == '2':
-                ans[i][j] = '='
-            elif s[i] == '2' and s[j] == '1':
-                ans[i][j] = '='
-            else:
-                cnt += 1
-    if cnt > 0:
-        if cnt%2 == 0:
-            if cnt == 2:
-                print('NO')
-                continue
-            else:
-                first = n
-                last = 0
-                for i in range(n):
-                    for j in range(n):
-                        if s[i] == '2' and s[j] == '2':
-                            if i < j:
-                                first = min(first, i)
-                                last = max(last, j)
-                                ans[i][j] = '+'
-                                ans[j][i] = '-'
-                ans[first][last] = '-'
-                ans[last][first] = '+'
-        else:
-            if cnt == 1:
-                print('NO')
-                continue
-            else:
-                first = n
-                last = 0
-                for i in range(n):
-                    for j in range(n):
-                        if s[i] == '2' and s[j] == '2':
-                            if i < j:
-                                first = min(first, i)
-                                last = max(last, j)
-                                ans[i][j] = '+'
-                                ans[j][i] = '-'
-                ans[first][last] = '-'
-                ans[last][first] = '+'
-
-    flag = True
-    for i in range(n):
-        if s[i] == '2' and '+' not in ans[i]:
-            flag = False
-            break
-    if not flag:
-        print('NO')
-        continue
-    print('YES')
-    for i in range(n):
-        print(''.join(ans[i]))
-
+    if 
 
             
