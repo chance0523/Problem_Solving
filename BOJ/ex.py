bca79f073191aa1e9113918bdeee5374292d8ffc
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-17 10:37:32 +0900

    0917

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,15 +1,3 @@
-nList = []
-for i in range(7):
-    nList.append(int(input()))
-
-s = 0
-m = 1000
-for i in range(7):
-    if nList[i] % 2:
-        m = min(m, nList[i])
-        s += nList[i]
-if s:
-    print(s)
-    print(m)
-else:
-    print(-1)
+s = input()
+for i in range(len(s)):
+    print(i)
