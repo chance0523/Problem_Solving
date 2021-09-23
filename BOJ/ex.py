769519bd1c8b39df68141b9afe22d1e633882ec3
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-23 20:40:38 +0900

    0923

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,7 +1,9 @@
-a,b = map(int,input().split())
-aSet = set(map(int,input().split()))
-bSet = set(map(int,input().split()))
-ab = aSet - bSet
-ba = bSet - aSet
-print(len(ab | ba))
-
+n = input().rstrip()
+f = int(input())
+new_n = n[:-2] + '00'
+new_n = int(new_n)
+for num in range(new_n, 2222222222):
+    if num%f == 0:
+        ans = str(num)[-2:]
+        print(ans)
+        break
