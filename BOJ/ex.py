8ed98b770089b119fcb3bb15bdfd2281725fd18e
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-25 11:12:50 +0900

    0925

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,26 +1,9 @@
-n, x = map(int, input().split())
-xList = list(map(int, input().split()))
-
-s = sum(xList[:x])
-l = 0
-r = x - 1
-M = 0
+n = int(input())
 cnt = 0
-while True:
-    if M < s:
-        cnt = 1
-        M = s
-    elif M == s:
-        cnt += 1
-    r += 1
-    if r == n:
-        break
-    s += xList[r]
-    s -= xList[l]
-    l += 1
-
-if M == 0:
-    print('SAD')
-else:
-    print(M)
-    print(cnt)
+for i in range(n, -1, -1):
+    ans = ''
+    for j in range(cnt):
+        ans += ' '
+    ans += '*' * (2 * i - 1)
+    print(ans)
+    cnt += 1
