b853f7e8558c2bb91765b20051faf307254666bf
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-27 10:09:03 +0900

    0927

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,9 +1,23 @@
-n = int(input())
+from collections import deque
+
+n, m = map(int,input().split())
+nList = list(map(int,input().split()))
+
+d = deque([i for i in range(1, n+1)])
+
 cnt = 0
-for i in range(n, -1, -1):
-    ans = ''
-    for j in range(cnt):
-        ans += ' '
-    ans += '*' * (2 * i - 1)
-    print(ans)
-    cnt += 1
+for num in nList:
+    while True:
+        if d[0] == num:
+            d.popleft()
+            break
+        else:
+            if d.index(num) < len(d) / 2:
+                while d[0] != num:
+                    d.append(d.popleft())
+                    cnt += 1
+            else:
+                while d[0] != num:
+                    d.appendleft(d.pop())
+                    cnt += 1
+print(cnt)
