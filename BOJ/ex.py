55c4983de3849ea27d74ac6dec39c0b1919f0802
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-09-30 08:27:36 +0900

    0930

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,10 +1,24 @@
-a,b = input().rstrip().split()
-
-m = 1e9
-for i in range(len(b) - len(a) + 1):
-    cnt = 0
-    for j in range(len(a)):
-        if a[j] != b[i + j]:
-            cnt += 1
-    m = min(m, cnt)
-print(m)
+while True:
+    n,m = map(int,input().split())
+    if n+m==0:
+        break
+    nList = [[0,i] for i in range(10001)]
+    for i in range(n):
+        kList = list(map(int,input().split()))
+        for k in kList:
+            nList[k][0] += 1
+    nList.sort(reverse=True)
+    M = nList[0][0]
+    M2 = 0
+    for i in range(len(nList)):
+        if nList[i][0] == M: # 1등
+            continue
+        else:
+            M2 = nList[i][0]
+            break
+    ans = []
+    for i in range(len(nList)):
+        if nList[i][0] == M2:
+            ans.append(nList[i][1])
+    ans.sort()
+    print(*ans)
