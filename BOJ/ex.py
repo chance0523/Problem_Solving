517a7ad32fe2572fd3972d017284b69b2bd7377f
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-03 21:09:37 +0900

    1003

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,17 +1,6 @@
-while True:
-    try:
-        a = input()
-        b = input()
-        alpha1 = [0 for i in range(26)]
-        alpha2 = [0 for i in range(26)]
-        for i in range(len(a)):
-            alpha1[ord(a[i]) - ord('a')] += 1
-        for i in range(len(b)):
-            alpha2[ord(b[i]) - ord('a')] += 1
-        ans = ''
-        for i in range(26):
-            cnt = min(alpha1[i], alpha2[i])
-            ans += chr(i+ord('a'))*cnt
-        print(ans) 
-    except EOFError:
-        break
+k = int(input())
+s = input()
+ans = ''
+for i in range(0, len(s), k):
+    ans += s[i]
+print(ans)
