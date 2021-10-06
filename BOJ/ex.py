efcc706b6ab20459c80757d637fc72de89a15b58
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-06 14:11:15 +0900

    1006

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,12 +1,2 @@
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-nList = [int(input()) for i in range(n)]
-nList.sort(reverse=True)
-ans = 0
-for i in range(n):
-    money = nList[i] - i
-    if money > 0:
-        ans += money
-print(ans)
+for _ in range(int(input())):
+    print(input().rstrip()[::-1])
