7fb31e8c3f27924c470f13f3031a4da84e3dcea6
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-08 11:31:36 +0900

    1008

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,5 +1,15 @@
-a = list(input())
-ans = 0
-for i in range(5):
-    ans += int(a[i])**5
-print(ans)
+n = int(input())
+M = 0
+Midx = 0
+for i in range(1, n+1):
+    nList = list(map(int,input().split()))
+    tM = 0
+    for a in range(3):
+        for b in range(a+1, 4):
+            for c in range(b+1, 5):
+                s = nList[a] + nList[b] + nList[c]
+                tM = max(tM, s%10)
+    if tM >= M:
+        M = tM
+        Midx = i
+print(Midx)
