8ba380fcd73da9d456238e2e689e16b5f1f66d95
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-17 23:20:18 +0900

    1017

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,17 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-k = 0
-nList = []
-
-for i in range(n):
-    nList.append(input().rstrip()[::-1]) # 귀찮으니까 뒤집어서 넣어준다
-
-for i in range(1, len(nList[0])+1): # 시간 충분.
-    nSet = set()
-    for j in range(n):
-        nSet.add(nList[j][:i])
-    if len(nSet) == n: # 중복되는게 없을때
-        print(i)
-        exit(0)
+for _ in range(int(input())):
+    k = int(input())
+    print(23*k)
