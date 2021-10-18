c40f91d721284d34c30b8f3a7f917b1baeac6fc2
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-18 09:26:30 +0900

    1018

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,6 +1,5 @@
-import sys
-input = sys.stdin.readline
-
-for _ in range(int(input())):
-    k = int(input())
-    print(23*k)
+n = int(input())
+nList = [float(input()) for _ in range(n)]
+for i in range(1, n):
+    nList[i] = max(nList[i], nList[i]*nList[i-1])
+print("%.3f" % (max(nList)))
