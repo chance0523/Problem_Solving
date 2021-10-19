53055eb2af95b4ff3bcea9d4581b1154b55a6ea2
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-20 08:49:56 +0900

    1020

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -1,5 +1,22 @@
+'''
+안녕하세요.
+제 코드를 봐주셔서 감사합니다.
+부족한 점들은 양해 부탁드립니다.
+'''
 for _ in range(int(input())):
-    a,b = input().split()
-    a = int(a, 2) # 이진수를 십진수로
-    b = int(b, 2)
-    print(bin(a+b)[2:]) # 십진수를 이진수로. 앞에 b2는 잘라줌
+    n = int(input())
+    nList = list(map(int,input().split()))
+
+    # 딕셔너리로 해도 되고, 1,000,001짜리 배열로 해도 된다
+    nDict = {}
+    for i in range(n):
+        nDict[nList[i]] = 1
+
+    m = int(input())
+    mList = list(map(int,input().split()))
+    
+    for i in range(m):
+        if mList[i] in nDict.keys():
+            print(1)
+        else:
+            print(0)
