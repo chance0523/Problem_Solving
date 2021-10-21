69650723fb7109d460d5e3fa559efe2afa79bcf5
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-21 21:02:22 +0900

    1021

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -3,20 +3,28 @@
 제 코드를 봐주셔서 감사합니다.
 부족한 점들은 양해 부탁드립니다.
 '''
+
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
-    n = int(input())
-    nList = list(map(int,input().split()))
-
-    # 딕셔너리로 해도 되고, 1,000,001짜리 배열로 해도 된다
+    nList = list(map(int,input().rstrip().split()))
+    n = nList[0] # Ti
+    nList = nList[1:] # Nij
     nDict = {}
-    for i in range(n):
-        nDict[nList[i]] = 1
+    flag = False # 과반수 먹은 나라가 없다.
 
-    m = int(input())
-    mList = list(map(int,input().split()))
-    
-    for i in range(m):
-        if mList[i] in nDict.keys():
-            print(1)
-        else:
-            print(0)
+    for k in nList:
+        try:
+            nDict[k] += 1
+            if nDict[k] > n//2: # 절반 초과
+                ans = k
+                flag = True
+                break
+        except:
+            nDict[k] = 1
+    if flag:
+        print(ans)
+    else:
+        print('SYJKGW')
+
