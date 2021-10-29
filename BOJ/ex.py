0add9d85f8d1217b0fe5879d3dc796890b731f2f
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-29 09:46:58 +0900

    1029

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -8,5 +8,29 @@ import sys
 input = sys.stdin.readline
 
 
-l = int(input())
-print(3 ** 0.5 / 4 * l ** 2)
+for t in range(int(input())):
+    s = input().rstrip()
+    alpha = [0 for i in range(26)]
+    aa = ord('a') # 귀찮으니 미리 만들어주자
+    zz = ord('z')
+    AA = ord('A')
+    ZZ = ord('Z')
+    for i in range(len(s)):
+        if aa <= ord(s[i]) <= zz or AA <= ord(s[i]) <= ZZ: # 알파벳일때만
+            alpha[ord(s[i].lower()) - aa] += 1 # 헷갈리니 소문자로 바꿔놓고 계산
+
+    m = 1e9
+    for i in range(26):
+        m = min(m, alpha[i])
+
+    ans = f'Case {t+1}: '
+    if m == 1:
+        ans += 'Pangram!'
+    elif m == 2:
+        ans += 'Double pangram!!'
+    elif m == 3:
+        ans += 'Triple pangram!!!'
+    else:
+        ans += 'Not a pangram'
+
+    print(ans)
