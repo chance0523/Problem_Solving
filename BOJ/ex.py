2c2ab885b5d194cdc010e6bce43674467bbfcf72
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-31 21:41:38 +0900

    1031

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -8,20 +8,19 @@ import sys
 input = sys.stdin.readline
 
 
-n = int(input())
-ans = 0
-# 선끼리 교차하지 않으려면 스택으로 바로바로...
-for _ in range(n):
-    s = input().rstrip()
-    stack = []
-    for i in range(len(s)):
-        if not stack: # 스택이 비어있으면 일단 넣어준다
-            stack.append(s[i])
-        else:
-            if stack[-1] == s[i]: # 스택의 top과 같으면 pop
-                stack.pop()
-            else:
-                stack.append(s[i]) # 아니면 넣어준다
-    if not stack: # 스택이 비어있으면 좋은 단어
-        ans += 1
-print(ans)
+a = 0
+b = 0
+a += 3 * int(input())
+a += 2 * int(input())
+a += 1 * int(input())
+
+b += 3 * int(input())
+b += 2 * int(input())
+b += 1 * int(input())
+
+if a > b:
+    print('A')
+elif a < b:
+    print('B')
+else:
+    print('T')
