d20a9d1d1bb8260baf69f1202301f2655477846e
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-07 15:16:06 +0900

    1107

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,7 +7,24 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-nList = [input().rstrip() for _ in range(n)]
-nList.sort(key=lambda x:float(x))
-print(nList[1])
+def isPalindrome(n):
+    s1 = str(n)
+    s2 = s1[::-1]
+    if s1 == s2:
+        return True
+    else:
+        return False
+
+def isPrime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+a,b = map(int,input().split())
+if b>11111111:
+    b = 11111111
+for i in range(a, b+1):
+    if isPalindrome(i) and isPrime(i):
+        print(i)
+print(-1)
