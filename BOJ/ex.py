a9a05ab6187ec816ecc4b1ce2e9743e96e402335
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-11 06:50:04 +0900

    1111

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -6,21 +6,13 @@
 
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 
-n,m = map(int,input().split())
-cList = list(combinations([i for i in range(m)], 3)) # 전체 경우
-nList = [list(map(int,input().rstrip().split())) for _ in range(n)] 
 
-ans = 0
-for c in cList:
-    cur = 0
-    for i in range(n):
-        temp = 0
-        for j in range(3):
-            temp = max(temp, nList[i][c[j]])
-        cur += temp
-    ans = max(ans, cur)
-
-print(ans)
-
+n = int(input())
+if n < 2:
+    print(1)
+else:
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = a + b + 1, a
+    print(a % 1000000007)
