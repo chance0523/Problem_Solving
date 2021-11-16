bd2fda16cc26cdef808420f1fa4553bdf9d15fbe
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-16 16:54:37 +0900

    1116

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -8,24 +8,13 @@ import sys
 input = sys.stdin.readline
 
 
-'''
-홀수가 하나라도 있으면 홀수끼리 곱한 것이 답이다
-짝수만 있으면 짝수끼리 곱한 것이 답
-'''
-
+n = int(input())
 nList = list(map(int,input().rstrip().split()))
-odd = []
-for i in range(3):
-    if nList[i] % 2 != 0:
-        odd.append(nList[i])
-
-if odd:
-    ans = 1
-    for i in range(len(odd)):
-        ans *= odd[i]
-    print(ans)
-else:
-    ans = 1
-    for i in range(len(nList)):
-        ans *= nList[i]
-    print(ans)
+dp = [0 for _ in range(n)]
+for i in range(1, n):
+    for k in range(1, i+2):
+        temp = nList[i-k+1:i+1]
+        if k == i+1:
+            k = i
+        dp[i] = max(dp[i], dp[i-k] + abs(max(temp)-min(temp)))
+print(dp[-1])
