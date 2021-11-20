34f49088bc17548e0b9c9ab2cfc89810d2c36ec2
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-20 10:52:41 +0900

    1120

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,28 +7,9 @@
 import sys
 input = sys.stdin.readline
 
-def func(a, b, op):
-    if op == '+':
-        return a+b
-    elif op == '-':
-        return a-b
-    elif op == '*':
-        return a*b
-    elif op == '/':
-        return -1*(abs(a)//abs(b)) if a*b < 0 else a//b
-
-sList = list(input().rstrip().split())
-a = int(sList[0])
-b = int(sList[2])
-c = int(sList[4])
-
-op1 = sList[1]
-op2 = sList[3]
-
-ans1 = func(func(a,b,op1), c, op2)
-ans2 = func(a, func(b, c, op2), op1)
-if ans1 > ans2: # 작은거부터 출력
-    ans1, ans2 = ans2, ans1
-
-print(ans1)
-print(ans2)
+n,m = map(int,input().split())
+ans = n
+while n:
+    n //= m
+    ans += n
+print(ans)
