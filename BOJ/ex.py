0a6d1a4173c94ac9b0d8bac8cb4c763c8b74a0c3
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-21 23:10:02 +0900

    1121

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,9 +7,18 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-ans = n
-while n:
-    n //= m
-    ans += n
+s = input().rstrip()
+nList = [input().rstrip() for _ in range(int(input()))]
+nList.sort()
+ansP = 0
+ans = nList[0]
+for i in range(len(nList)):
+    L = s.count('L') + nList[i].count('L')
+    O = s.count('O') + nList[i].count('O')
+    V = s.count('V') + nList[i].count('V')
+    E = s.count('E') + nList[i].count('E')
+    p = ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100
+    if ansP < p:
+        ansP = p
+        ans = nList[i]
 print(ans)
