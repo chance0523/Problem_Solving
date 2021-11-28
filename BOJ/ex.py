e84fea5e126275f0a0728851970592d2d3278a59
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-28 12:12:33 +0900

    1128

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,7 +7,16 @@
 import sys
 input = sys.stdin.readline
 
-b1 = input()
-b2 = input()
-ans = bin(int(b1, 2) * int(b2, 2))[2:]
-print(ans)
+n, k = map(int,input().split())
+kList = [int(input()) for _ in range(n)]
+dp = [0 for _ in range(k + 1)]
+for i in range(1, k+1):
+    temp = []
+    for j in kList:
+        if j <= i and dp[i-j] != -1:
+            temp.append(dp[i-j])
+    if not temp:
+        dp[i] = -1
+    else:
+        dp[i] = min(temp) +1
+print(dp[k])
