43ce0459591464f034abc7f8c7cfd4a351ae5944
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-29 15:03:23 +0900

    1129

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -6,17 +6,18 @@
 
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-n, k = map(int,input().split())
-kList = [int(input()) for _ in range(n)]
-dp = [0 for _ in range(k + 1)]
-for i in range(1, k+1):
-    temp = []
-    for j in kList:
-        if j <= i and dp[i-j] != -1:
-            temp.append(dp[i-j])
-    if not temp:
-        dp[i] = -1
-    else:
-        dp[i] = min(temp) +1
-print(dp[k])
+n = int(input())
+q = deque(enumerate(map(int,input().rstrip().split())))
+ans = []
+
+while q:
+    idx, num = q.popleft()
+    ans.append(idx + 1)
+    if num > 0:
+        q.rotate(-(num - 1))
+    elif num < 0:
+        q.rotate(-num)
+
+print(*ans)
