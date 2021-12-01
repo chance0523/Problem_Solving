629a1a191d37db8ceb0a6d274257af1117a245f3
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-01 09:27:25 +0900

    1201

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,10 +7,20 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-if n == 0:
-    print('divide by zero')
+y1, m1, d1 = map(int, input().split())
+y2, m2, d2 = map(int, input().split())
+man_old = 0
+if m1 < m2:
+    man_old = y2-y1
+elif m1 == m2:
+    if d1 <= d2:
+        man_old = y2-y1
+    else:
+        man_old = y2-y1-1
 else:
-    nList = list(map(int,input().rstrip().split()))
-    ans = sum(nList)/n / (sum(nList)/n)
-    print("%.2f" % ans)
+    man_old = y2-y1-1
+count_old = y2-y1+1
+year_old = y2-y1
+print(man_old)
+print(count_old)
+print(year_old)
