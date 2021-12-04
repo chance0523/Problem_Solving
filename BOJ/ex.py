f7cc2747965fe297002f14445d5c9fb27e839927
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-04 23:14:34 +0900

    1204

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,11 +7,4 @@
 import sys
 input = sys.stdin.readline
 
-s = input().rstrip().split()
-a = int(s[0])
-b = int(s[2])
-c = int(s[4])
-if a+b == c:
-    print('YES')
-else:
-    print('NO')
+print(-1)
