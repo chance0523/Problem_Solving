687ca28e46688de8e98c5df5f93d9c7e86686621
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-18 10:58:23 +0900

    1218

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -8,10 +8,12 @@ import sys
 input = sys.stdin.readline
 
 
-n = int(input())
-if n%2 == 1:
-    print(0)
-elif n//2 % 2 == 0:
-    print(2)
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if (a==8 or a==9) and (d==8 or d==9) and b == c:
+    print('ignore')
 else:
-    print(1)
+    print('answer')
