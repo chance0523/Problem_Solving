581dd7f40a5cb99dbd940277179b8738cb3b1758
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-19 23:20:33 +0900

    1219

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -8,12 +8,14 @@ import sys
 input = sys.stdin.readline
 
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-
-if (a==8 or a==9) and (d==8 or d==9) and b == c:
-    print('ignore')
-else:
-    print('answer')
+limit = int(input())
+speed = int(input())
+d = speed - limit
+if d <= 0:
+    print("Congratulations, you are within the speed limit!")
+elif d >= 1 and d <= 20:
+    print("You are speeding and your fine is ${}.".format(100))
+elif d >= 21 and d <= 30:
+    print("You are speeding and your fine is ${}.".format(270))
+elif d >= 31:
+    print("You are speeding and your fine is ${}.".format(500))
