ea933366c9b18a24126709d376d424273cea6980
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-20 09:03:28 +0900

    1220

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,15 +7,8 @@
 import sys
 input = sys.stdin.readline
 
-
-limit = int(input())
-speed = int(input())
-d = speed - limit
-if d <= 0:
-    print("Congratulations, you are within the speed limit!")
-elif d >= 1 and d <= 20:
-    print("You are speeding and your fine is ${}.".format(100))
-elif d >= 21 and d <= 30:
-    print("You are speeding and your fine is ${}.".format(270))
-elif d >= 31:
-    print("You are speeding and your fine is ${}.".format(500))
+n = int(input())
+k = int(input())
+x = min(k + 60, n)
+res = x*1500 + (n-x)*3000
+print(res)
