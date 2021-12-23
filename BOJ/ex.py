35c8eae2b3cefd3b230160c7dd3935068be0d763
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-23 17:41:58 +0900

    1223

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,9 +7,5 @@
 import sys
 input = sys.stdin.readline
 
-nList = list(map(int,input().rstrip().split()))
-nList.sort()
-if nList[0] == nList[1] or nList[1] == nList[2] or nList[0]+nList[1] == nList[2]:
-    print('S')
-else:
-    print('N')
+k, w, m = map(int, input().split())
+print((w-k)//m + (1 if (w-k)%m else 0))
