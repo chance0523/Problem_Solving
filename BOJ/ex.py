46b3202fbc1092ed3b4e38cb8f125bde42b6a1ec
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-28 12:00:23 +0900

    1228

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,11 +7,8 @@
 import sys
 input = sys.stdin.readline
 
-h1, m1, s1 = map(int, input().split(':'))
-h2, m2, s2 = map(int, input().split(':'))
-t1 = h1*3600 + m1*60 + s1
-t2 = h2*3600 + m2*60 + s2
-if t1 > t2:
-    print(t2-t1+3600*24)
+a,b = map(int,input().split())
+if a>=b:
+    print(b)
 else:
-    print(t2-t1)
+    print(a+1)
