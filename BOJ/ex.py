0094ee952e1c03b17de7456702a0c675664d22db
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-12-29 09:20:16 +0900

    1229

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,8 +7,8 @@
 import sys
 input = sys.stdin.readline
 
-a,b = map(int,input().split())
-if a>=b:
-    print(b)
-else:
-    print(a+1)
+h, w = map(int, input().split())
+a, b = max(h, w), min(h, w)
+l1 = a/3 if a/3 <= b else b
+l2 = b/2
+print(max(l1, l2))
