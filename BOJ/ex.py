6b17022ce002a28700a0f43049cc25a559f40842
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2022-01-08 10:59:30 +0900

    0108

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,6 +7,8 @@
 import sys
 input = sys.stdin.readline
 
-n, h, v = map(int, input().split())
-ans = max(h, n-h) * max(v, n-v)
-print(ans*4)
+N, A, B, C, D = map(int, input().split())
+A = (N//A + (1 if N%A else 0))*B
+C = (N//C + (1 if N%C else 0))*D
+
+print(min(A, C))
