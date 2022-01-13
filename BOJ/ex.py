ec5d5df2f2f1811062a85d15581f74655c787a7e
서정준 <51807128+chance0523@users.noreply.github.com>
2022-01-13 09:41:10 +0900

    0113

--- BOJ/ex.py
+++ BOJ/ex.py
@@ -7,4 +7,7 @@
 import sys
 input = sys.stdin.readline
 
-print(abs(int(input())- int(input())))
+S = int(input())
+M = int(input())
+L = int(input())
+print("happy" if S*1 + M*2 + L*3 >= 10 else "sad")
